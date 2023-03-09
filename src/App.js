481b2ffa4afe1d31e82cce259460ef24b0dc3770
haroldmud <haroldmudosa@gmail.com>
2023-03-09 17:05:20 +0200

    Feat: Get done with the game

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Dice from "./components/dice"
+
+
+export default function App() {
+  
+  return (
+    <section className="flex flex-col justify-center h-[100vh]">
+      <section className="w-[25rem] h-[30rem] border-[2rem] border-blues rounded-2xl mx-auto ">
+        
+        <div>
+          <h2 className="text-center text-2xl mt-4 font-bold text-blue">Tenzies</h2>
+          <p className="text-gray-400 px-10 text-center">Roll untill all dice are the same. click each dice to freeze it at its current value between rolls.</p>
+        </div>
+        
+        <Dice/>
+        
+      </section>
+    </section>
+  )
+}
---
+++ src/components/dice.js
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+
+export default function Dice(){
+  const [collection, setCollection] = useState(provider());
+
+  function diceGenerator(){
+    return {number : Math.ceil(Math.random()*6), isHeld:false, id: nanoid()}
+  }
+
+  function provider(){
+    let Arr = [];
+    for(let i=0; i<10;i++){
+      Arr.push(diceGenerator());
+    }
+    return Arr;
+  }
+
+  function rollDice(){
+    setCollection(dice => dice.map(die => {
+      return die.isHeld ? die : diceGenerator()
+    }))
+  }
+
+  function holdDice(id){
+    setCollection(
+      oldDice => oldDice.map(die => {
+        return die.id === id ? 
+            {...die, isHeld: !die.isHeld} :
+            die
+    })
+    )
+  }
+  function pusher(){
+    const newArr = [];
+    for(let i=0; i < collection.length; i++){
+      if(collection[i].isHeld === true){
+        newArr.push(collection[i].number)
+      }
+    }
+    return newArr;
+  }
+  const again= pusher();
+  function equality(){
+      for(let i =0; i< again.length;i++){
+        if(again[i] !== again[0]){
+          return false;
+        }
+      }
+      return true;
+    }
+
+  useEffect(()=>{
+    console.log(again);
+  })
+  
+  function restart(){
+    return window.location.reload();
+  }
+  
+  return(
+    <section className="relative">
+    <div className="flex justify-center h-12 mt-6"> 
+    {
+         (equality() && again.length === 10) ? <p className={`mx-auto text-green-500 font-bold`}>YOU WON BWAY!!!</p>
+        : (equality() === false && again.length === 10) ?  <p className={`mx-auto text-red-500 font-bold`}>YOU LOST </p> : <p className="font-bold">YOU CAN PLAY</p>
+    }
+        </div>
+        <div className="flex justify-center">
+        </div>
+    <div className=" grid grid-cols-5 w-fit gap-2 mx-auto mt-8">
+      {
+        collection.map((item, idx)=> 
+          <button key={item.id}
+            onClick={()=>
+              holdDice(item.id)
+            }
+          className={`text-blue border ${item.isHeld ? "bg-held" : ""} w-fit py-2 px-4 h-fit rounded-xl shadow`}>{item.number}</button>
+        )
+      }
+    </div>{
+    again.length === 10 && <div className="top-[5rem] bg-transparent h-[6rem] w-full absolute"></div>
+    }
+    <div className="w-full  flex justify-center mt-8">
+      {
+        again.length === 10 ? <button onClick={restart} className="w-fit bg-blue-700 px-3 py-1 rounded text-white font-bold mx-auto">Restart</button> : <button onClick={()=>rollDice()} className="w-fit bg-blue-700 px-3 py-1 rounded text-white font-bold mx-auto">Roll</button>
+      }
+    </div>
+    
+    </section>
+  )
+}
